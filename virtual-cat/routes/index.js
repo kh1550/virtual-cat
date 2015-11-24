9a669444c768e7ddf8eac44bf969c072e592428b
kh1550 <kristin.dramedij60@gmail.com>
2015-11-24 16:20:45 -0500

    tried switching to sqlite3

--- virtual-cat/routes/index.js
+++ virtual-cat/routes/index.js
@@ -6,20 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+router.post('/', function(req,res,next) {
+  res.redirect('/play');
+});
+
 router.post('/name', function(req, res, next) {
- 	res.redirect('play');
+  res.redirect('play');
 });
 
 router.get('/name', function(req, res, next) {
-	res.send("name your cat");
+  res.send("name your cat");
 });
 
 router.get('/play', function(req, res, next) {
-	res.send("play with cat");
+  res.send("play with cat");
 });
 
 router.get('/cafe/:cafe_id', function(req, res, next) {
-	res.send("cat cafe");
+  res.send("cat cafe");
 });
 
 module.exports = router;
