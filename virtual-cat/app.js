4d7ed65dfe6b449616b7a0002f466f1284d6db11
kh1550 <kristin.dramedij60@gmail.com>
2015-12-01 00:31:32 -0500

    bootstrap and buttons

--- virtual-cat/app.js
+++ virtual-cat/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var session = require('express-session');
+require('sass');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
